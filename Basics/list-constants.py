4f895be0301610156357602569ed8905aec4c95e
Ahmed <ahmn21@hotmail.com>
2020-09-03 17:20:45 +0500

    lists

---
+++ Basics/list-constants.py
@@ -0,0 +1,14 @@
+"""
+Data in the list can be different and comma seprates the values.
+We can also do list inside list on line #12. It has three elements
+List can be empty. Lists are mutable, strings are not.
+"""
+print(['r', 'g', 'b'])
+
+print([1, 23, 34])
+
+print(['rwer', 234, 7834.23])
+
+print(['r', [12,12.4], 'b'])
+
+print([])
---
+++ Lists/range-function.py
@@ -0,0 +1,8 @@
+#Range function returns a list of numbers to the list or generates it that ranges
+#from zero to 1 less than the parameter
+
+alpbhabet = ['a', 'b', 'c', 'x', 'z']
+
+print(list(range(3)))
+
+print(list(range(len(alpbhabet))))
