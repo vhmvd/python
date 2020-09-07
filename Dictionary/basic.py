c95ae351da2453dbda22d1197baee8902c3c1ffb
Ahmed <ahmn21@hotmail.com>
2020-09-07 15:33:21 +0500

    dictionary starter

---
+++ Dictionary/basic.py
@@ -0,0 +1,20 @@
+
+#init key value pair obj with the contructor
+bag = dict()
+
+#left side is key and right side is value
+
+bag['money'] = 23
+bag['chocolate'] = 32
+bag['gun'] = 1
+
+print(bag)
+
+#to find the specific the specific item
+
+print(bag['gun'])
+
+#dictionary contents are mutable therefore they can be changed
+
+bag['gun'] = bag['gun'] + 419
+print(bag)
