f9e78b6964b65c50dcfcaec2e781e741926b235e
Ahmed <ahmn21@hotmail.com>
2020-09-07 16:19:24 +0500

    com

--- Dictionary/dictionary-assigned.py
+++ Dictionary/dictionary-assigned.py
@@ -1,5 +1,7 @@
 #assigning dictionay to a variable
+
+#key value pair used here are <strings> and <int>
+#can be changed <datatype> <datatype>
 bag = {'hello':1, 'jello':27, "skdjflk":45}
 print(bag)
-
 print(bag['jello'])
