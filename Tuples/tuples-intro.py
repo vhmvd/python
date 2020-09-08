efe29ef3cd18edb8b61a887ef6abffbc61d58a5b
Ahmed <ahmn21@hotmail.com>
2020-09-08 19:44:05 +0500

    tupes

--- Tuples/tuples-intro.py
+++ Tuples/tuples-intro.py
@@ -1,6 +1,6 @@
 #Tuples are a set of items mathematically.
 #They are like lists but unchangeable 
-
+#Has to be in paranthesis
 #Strings and tuples are immutable
 
 #Tuples cannot be sorted and appended
