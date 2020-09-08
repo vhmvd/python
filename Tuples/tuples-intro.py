531b1cb0a48f9301353aa9c069dc49e1d6d4fc41
Ahmed <ahmn21@hotmail.com>
2020-09-08 17:27:20 +0500

    tuples

---
+++ Tuples/tuples-intro.py
@@ -0,0 +1,8 @@
+#Tuples are a set of items mathematically.
+#They are like lists but unchangeable 
+
+#Strings and tuples are immutable
+
+#Tuples cannot be sorted and appended
+
+x = ('hello', 'jello', 'mellow')
