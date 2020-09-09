60be059bc0fbbbd6a3bf7b817dc1be06c4181517
Ahmed <ahmn21@hotmail.com>
2020-09-10 01:53:33 +0500

    ex3

---
+++ String/ex3.py
@@ -0,0 +1,10 @@
+"""
+Write code that uses iteration to print out the length of each element of the list stored in str_list.
+"""
+
+str_list = ["hello", "", "goodbye", "wonderful", "I love Python"]
+
+# Write your code here.
+
+for itr in str_list:
+    print(len(itr))
