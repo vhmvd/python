97e3848a23fabcbacabcc4bfe50ee7dd80ff55a5
Ahmed <ahmn21@hotmail.com>
2020-09-09 19:44:49 +0500

    turtules

--- Tuples/exercise.py
+++ Tuples/exercise.py
@@ -1,6 +1,6 @@
 """
-10.2 Write a program to read through the mbox-short.txt and figure out the 
-distribution by hour of the day for each of the messages. You can pull the 
+10.2 Write a program to read through the mbox-short.txt and figure out the
+distribution by hour of the day for each of the messages. You can pull the
 hour out from the 'From ' line by finding the time and then splitting the string a second time using a colon.
 From stephen.marquard@uct.ac.za Sat Jan  5 09:14:16 2008
 Once you have accumulated the counts for each hour, print out the counts, sorted by hour as shown below.
@@ -8,6 +8,7 @@ Once you have accumulated the counts for each hour, print out the counts, sorted
 
 fileHandle = open('mbox-short.txt')
 hoursAndCount = dict()
+
 for line in fileHandle:
     if line.startswith('From '):
         lineSplit = line.split()
---
+++ Turtles/basic-turtle.py
@@ -0,0 +1,8 @@
+import turtle
+
+graphicWindow = turtle.Screen()
+me = turtle.Turtle()
+me.forward(100)
+me.left(90)
+me.forward(100)
+turtle.done()
