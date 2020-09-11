223c4be344cbf5d48a0f479a11f241e361c5ac2a
Ahmed <ahmn21@hotmail.com>
2020-09-11 15:29:14 +0500

    ref and ptr

--- Obj & Ref/ref.py
+++ Obj & Ref/ref.py
@@ -3,5 +3,13 @@ b = 'abc'
 
 print(a is b)
 print(a == b)
-print(id(a))
-print(id(b))
+print(hex(id(a)))
+print(hex(id(b)))
+
+a1 = ['1', 12]
+b1 = ['1', 12]
+
+print(a1 is b1) #a1 is not b1 because they are at different location in memory
+print(a1 == b1)
+print(hex(id(a1)))
+print(hex(id(b1)))
